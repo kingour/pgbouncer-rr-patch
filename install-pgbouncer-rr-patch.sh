1eabb9ff6abab7dfa6619588946106894a40a156
Kingour <kingour1986@gmail.com>
2025-12-09 15:46:13 +0800

    Bump pgbouncer to v1.23.1

--- install-pgbouncer-rr-patch.sh
+++ install-pgbouncer-rr-patch.sh
@@ -76,8 +76,8 @@ if [ $patchstatus -eq 1 ]; then
    echo "Possible causes: "
    echo "   pgbouncer-rr-patch already installed in target directory?"
    echo "   new version of pgbouncer with changed source files that can't be patched?"
-   echo "      - last tested with pgbouncer v1.12 (April 2020)"
-   echo "      - Try getting pgbouncer with: git clone https://github.com/pgbouncer/pgbouncer.git --branch \"stable-1.19\""
+   echo "      - last tested with pgbouncer v1.23.1"
+   echo "      - Try getting pgbouncer with: git clone https://github.com/pgbouncer/pgbouncer.git --branch \"stable-1.23\""
    echo "Status: pgbouncer-rr-patch merge FAILED"
 else
    echo "Status: pgbouncer-rr-patch merge SUCEEDED"
--- src/pycall.c
+++ src/pycall.c
@@ -1,17 +1,6 @@
 /*
-Copyright 2015-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Licensed under the Amazon Software License (the "License").
-You may not use this file except in compliance with the License. A copy of the License is located at
-
-    http://aws.amazon.com/asl/
-
-or in the "license" file accompanying this file.
-This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
-/*
- * pgbouncer-rr extension: call external python function
+ * pycall.c — Compatible with Python 2.7 and Python 3.x
+ * For pgbouncer-rr extension (query rewriting / routing)
  */
 
 #include <Python.h>
@@ -19,125 +8,133 @@ This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS O
 #include <usual/pgutil.h>
 
 char *pycall(PgSocket *client, char *username, char *query_str, int in_transaction,
-        char *py_file, char* py_function) {
-	PyObject *pName = NULL, *pModule = NULL, *pFunc = NULL;
-	PyObject *pArgs = NULL, *pValue = NULL;
-	PyObject *ptype, *perror, *ptraceback, *bytes_obj, *string_obj, *objInTransaction;
-	char *py_file_copy, *py_module, *ext;
-	char *res = NULL;
-	int agCount = 0;
-	const char *dbname = NULL;
+             char *py_file, char *py_function) {
 
-	/* setup python search path */
-	py_file_copy = strdup(py_file);
-	if (py_file_copy == NULL) {
-		slog_error(client, "out of memory");
-		return NULL;
-	}
-	setenv("PYTHONPATH", dirname(py_file_copy), 1);
+    PyObject *pName = NULL, *pModule = NULL, *pFunc = NULL;
+    PyObject *pArgs = NULL, *pValue = NULL;
+    PyObject *ptype, *perror, *ptraceback;
+    char *res = NULL, *ext = NULL, *py_file_copy = NULL, *py_module = NULL, *py_path_env = NULL;
 
-	/* setup python module name, function name */
-	strcpy(py_file_copy, py_file);
-	py_module = (char *) basename(py_file_copy);
-	ext = strrchr(py_module, '.');
-	if (ext)
-		ext[0] = '\0';
-
-	/* Initialize the Python interpreter
-	 * NOTE: This call is a no-op on subsequent calls, as we do not
-	 * call PyFinalize(). This
-	 * a) avoids the overhead of repeatedly reloading the interpreter
-	 * b) allows the use of global variables for persisting data in the
-	 *    routing / rewriting functions between calls.
-	 */
-	Py_Initialize();
-
-	/* Load python module */
-	pName = PyUnicode_FromString(py_module);
-	if (pName == NULL) {
-		slog_error(client, "Python module <%s> did not load", py_module);
-		goto finish;
-	}
-	pModule = PyImport_Import(pName);
-	if (pModule == NULL) {
-		slog_error(client, "Python module <%s> did not load", py_module);
-		goto finish;
-	}
-
-	/* Prepare to call python function */
-	pFunc = PyObject_GetAttrString(pModule, py_function);
-	if (!pFunc) {
-		slog_error(client, "Python Function <%s> not found in module <%s>",
-				py_function, py_module);
-		goto finish;
-	}
-	if (!PyCallable_Check(pFunc)) {
-		slog_error(client,
-				"Python Function <%s> in module <%s> is not callable!",
-				py_function, py_module);
-		goto finish;
-	}
-
-	/* Call function with two arguments - username and query_str */
-	pArgs = PyTuple_New(3);
-	if (pArgs == NULL) {
-		slog_error(client, "Python module <%s>: out of memory", py_module);
-		goto finish;
-	}
-	pValue = PyUnicode_FromString(username);
-	if (pValue == NULL) {
-		slog_error(client, "Python module <%s>: out of memory", py_module);
-		goto finish;
-	}
-	PyTuple_SetItem(pArgs, 0, pValue);
-	pValue = PyUnicode_DecodeUTF8(query_str, strlen(query_str), "ignore");
-	if (pValue == NULL) {
-		slog_error(client, "Python module <%s>: out of memory", py_module);
-		goto finish;
-	}
-	PyTuple_SetItem(pArgs, 1, pValue);
-
-	/* If there is a dbname argument, add it to the tuple */
-	if (agCount == 3) {
-        pValue = PyUnicode_FromString(dbname);
-		PyTuple_SetItem(pArgs, 2, pValue);
-		if (pValue == NULL) {
-			slog_error(client, "Python module <%s>: out of memory", py_module);
-			goto finish;
-		}
+    /* --- Setup python path --- */
+    py_file_copy = strdup(py_file);
+    if (!py_file_copy) {
+        slog_error(client, "Out of memory");
+        return NULL;
     }
-	objInTransaction = in_transaction? Py_True : Py_False;
-	Py_INCREF(objInTransaction);
-	PyTuple_SetItem(pArgs, 2, objInTransaction);
-	pValue = PyObject_CallObject(pFunc, pArgs);
-	if (pValue == NULL) {
-		slog_error(client, "Python Function <%s> failed to return a value",
-				py_function);
-		goto finish;
-	}
-	if (PyUnicode_Check(pValue)) {
-            bytes_obj = PyUnicode_AsUTF8String(pValue);
-            res = strdup(PyBytes_AsString(bytes_obj));
-            Py_DECREF(bytes_obj);
+
+    char *dir = dirname(py_file_copy);
+    py_path_env = malloc(strlen(dir) + 20);
+    if (!py_path_env) {
+        slog_error(client, "Out of memory");
+        free(py_file_copy);
+        return NULL;
+    }
+
+    sprintf(py_path_env, "PYTHONPATH=%s", dir);
+    putenv(py_path_env);
+
+    /* --- Derive module name --- */
+    char *file_tmp = strdup(py_file);
+    py_module = basename(file_tmp);
+    ext = strrchr(py_module, '.');
+    if (ext) *ext = '\0';
+
+    /* --- Initialize Python --- */
+    Py_Initialize();
+
+    /* --- Load module --- */
+#if PY_MAJOR_VERSION >= 3
+    pName = PyUnicode_FromString(py_module);
+#else
+    pName = PyString_FromString(py_module);
+#endif
+    if (!pName) {
+        slog_error(client, "Python module <%s> did not load", py_module);
+        goto finish;
+    }
+
+    pModule = PyImport_Import(pName);
+    if (!pModule) {
+        slog_error(client, "Python module <%s> did not load", py_module);
+        goto finish;
+    }
+
+    /* --- Get target function --- */
+    pFunc = PyObject_GetAttrString(pModule, py_function);
+    if (!pFunc || !PyCallable_Check(pFunc)) {
+        slog_error(client, "Python Function <%s> not found or not callable in <%s>", py_function, py_module);
+        goto finish;
+    }
+
+    /* --- Prepare args: username, query_str, in_transaction --- */
+    pArgs = PyTuple_New(3);
+    if (!pArgs) {
+        slog_error(client, "Out of memory creating args");
+        goto finish;
+    }
+
+#if PY_MAJOR_VERSION >= 3
+    PyTuple_SetItem(pArgs, 0, PyUnicode_FromString(username));
+    PyTuple_SetItem(pArgs, 1, PyUnicode_DecodeUTF8(query_str, strlen(query_str), "ignore"));
+#else
+    PyTuple_SetItem(pArgs, 0, PyString_FromString(username));
+    PyTuple_SetItem(pArgs, 1, PyString_FromString(query_str));
+#endif
+    PyTuple_SetItem(pArgs, 2, in_transaction ? Py_True : Py_False);
+    Py_INCREF(in_transaction ? Py_True : Py_False);
+
+    /* --- Call the Python function --- */
+    pValue = PyObject_CallObject(pFunc, pArgs);
+    if (!pValue) {
+        slog_error(client, "Python Function <%s> failed to return a value", py_function);
+        goto finish;
+    }
+
+    /* --- Convert Python return to char* --- */
+#if PY_MAJOR_VERSION >= 3
+    if (PyUnicode_Check(pValue)) {
+        PyObject *utf8 = PyUnicode_AsUTF8String(pValue);
+        res = strdup(PyBytes_AsString(utf8));
+        Py_DECREF(utf8);
+    } else if (PyBytes_Check(pValue)) {
+        res = strdup(PyBytes_AsString(pValue));
     } else {
-            res = NULL;
+        res = NULL;
     }
+#else
+    if (PyString_Check(pValue)) {
+        res = strdup(PyString_AsString(pValue));
+    } else if (PyUnicode_Check(pValue)) {
+        PyObject *utf8 = PyUnicode_AsUTF8String(pValue);
+        res = strdup(PyString_AsString(utf8));
+        Py_DECREF(utf8);
+    } else {
+        res = NULL;
+    }
+#endif
 
-    finish:
+finish:
     if (PyErr_Occurred()) {
-            PyErr_Fetch(&ptype, &perror, &ptraceback);
-            PyErr_NormalizeException(&ptype, &perror, &ptraceback);
-            string_obj = PyObject_Repr(perror);
-            bytes_obj = PyUnicode_AsUTF8String(string_obj);
-            slog_error(client, "Python error: %s", PyBytes_AsString(bytes_obj));
-            Py_DECREF(string_obj);
+        PyErr_Fetch(&ptype, &perror, &ptraceback);
+        PyObject *repr = PyObject_Str(perror);
+#if PY_MAJOR_VERSION >= 3
+        PyObject *utf8 = PyUnicode_AsUTF8String(repr);
+        slog_error(client, "Python error: %s", PyBytes_AsString(utf8));
+        Py_DECREF(utf8);
+#else
+        slog_error(client, "Python error: %s", PyString_AsString(repr));
+#endif
+        Py_XDECREF(repr);
     }
-	free(py_file_copy);
-	Py_XDECREF(pName);
-	Py_XDECREF(pModule);
-	Py_XDECREF(pFunc);
-	Py_XDECREF(pArgs);
-	Py_XDECREF(pValue);
-	return res;
-}
 
+    /* --- Cleanup --- */
+    free(py_file_copy);
+    free(file_tmp);
+    free(py_path_env);
+    Py_XDECREF(pName);
+    Py_XDECREF(pModule);
+    Py_XDECREF(pFunc);
+    Py_XDECREF(pArgs);
+    Py_XDECREF(pValue);
+    return res;
+}
